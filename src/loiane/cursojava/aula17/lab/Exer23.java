b5f5054a156de4c5f25e9a910f118ef52f9fac6e
Felipe Gui <fguiengenharia@gmail.com>
2023-08-24 11:21:13 -0300

    Exercícios de While/For/Do While - Exer23

---
+++ src/loiane/cursojava/aula17/lab/Exer23.java
@@ -0,0 +1,14 @@
+package loiane.cursojava.aula17.lab;
+
+public class Exer23 {
+
+	public static void main(String[] args) {
+		System.out.println("Stores almost two - Price list");
+		
+		for (int i = 1; i <= 50; i++) {
+			System.out.println(i + " - R$ " + (1.99*i));
+		}
+
+	}
+
+}
