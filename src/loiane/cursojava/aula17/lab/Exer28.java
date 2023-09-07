ddcc05d68983724f4c0afd0570c2e098c6ac5c65
Felipe Gui <fguiengenharia@gmail.com>
2023-09-07 11:04:25 -0300

    Exercícios de While/For/Do While - Exer28

---
+++ src/loiane/cursojava/aula17/lab/Exer28.java
@@ -0,0 +1,29 @@
+package loiane.cursojava.aula17.lab;
+
+import java.util.Scanner;
+
+public class Exer28 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Enter a number: ");
+		int num = scan.nextInt();
+
+		boolean prime = true;
+
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				System.out.println("It's not a prime number because it's divisible by: " + i);
+				prime = false;
+			}
+		}
+
+		if (prime) {
+			System.out.println("Is prime number");
+		}
+		scan.close();
+
+	}
+
+}
