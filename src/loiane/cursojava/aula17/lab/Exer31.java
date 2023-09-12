667dccdb0b1c303ace09b8eaa875cad6c3454b01
Felipe Gui <fguiengenharia@gmail.com>
2023-09-12 09:50:40 -0300

    Exercícios de While/For/Do While - Exer31 - método  add DecimalFormat

--- src/loiane/cursojava/aula17/lab/Exer31.java
+++ src/loiane/cursojava/aula17/lab/Exer31.java
@@ -1,5 +1,6 @@
 package loiane.cursojava.aula17.lab;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Exer31 {
@@ -13,12 +14,14 @@ public class Exer31 {
 		
 		salary += (salary / 100) * percentage;
 		
+		DecimalFormat format = new DecimalFormat("###,###.##");
+		
 		for (int i = 1997; i <= 2015; i++) {
 			percentage *= 2;
 			
 			salary += (salary / 100) * percentage;
 			
-			System.out.println(i + " = " + salary + " - " + percentage + "%");
+			System.out.println(i + " = " + format.format(salary) + " - " + percentage + "%");
 		}
 		
 		scan.close();
