5cc76db0c493be61840adc41536a94fb63814fdb
Felipe Gui <fguiengenharia@gmail.com>
2023-09-21 12:39:59 -0300

    Exercício de Array - Exer02

---
+++ src/loiane/cursojava/aula19/lab/Exer02.java
@@ -0,0 +1,36 @@
+package loiane.cursojava.aula19.lab;
+
+import java.util.Scanner;
+
+public class Exer02 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int[] vetA = new int[8];
+		int[] vetB = new int[vetA.length];
+
+		for (int i = 0; i < vetA.length; i++) {
+			System.out.println("Enter the position value: " + i);
+			vetA[i] = scan.nextInt();
+
+			vetB[i] = vetA[i] * 2;
+		}
+
+		System.out.print("Array A = ");
+		for (int i = 0; i < vetA.length; i++) {
+			System.out.print(vetA[i] + " ");
+		}
+
+		System.out.println();
+
+		System.out.print("Array B = ");
+		for (int i = 0; i < vetB.length; i++) {
+			System.out.print(vetB[i] + " ");
+		}
+
+		scan.close();
+
+	}
+
+}
