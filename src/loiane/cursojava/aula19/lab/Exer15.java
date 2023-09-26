ae0831cdde7ab181ede0b5b5432f8d6f15d39126
Felipe Gui <fguiengenharia@gmail.com>
2023-09-26 09:44:27 -0300

    Exercício de Array - Exer15

---
+++ src/loiane/cursojava/aula19/lab/Exer15.java
@@ -0,0 +1,45 @@
+package loiane.cursojava.aula19.lab;
+
+import java.util.Scanner;
+
+public class Exer15 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int[] vetA = new int[4];
+
+		for (int i = 0; i < vetA.length; i++) {
+			System.out.println("Insert the value position: " + i);
+			vetA[i] = scan.nextInt();
+		}
+		
+		int pair = 0;
+		for (int i = 0; i < vetA.length; i++) {
+			
+			if (vetA[i] % 2 == 0) {
+				pair ++; 
+			}
+		}
+		
+		int odd = vetA.length - pair;
+		
+		double percentageOdd = (odd * 100) / vetA.length;
+		
+		double percentagePair = 100 - percentageOdd;
+		
+		System.out.print("Array = ");
+		for (int i = 0; i < vetA.length; i++) {
+			System.out.print(vetA[i] + " ");
+		}
+		
+		System.out.println();
+		
+		System.out.println("Percentage Pair: " + percentagePair + "%");
+		System.out.println("Percentage Odd: " + percentageOdd + "%");
+		
+		scan.close();
+
+	}
+
+}
